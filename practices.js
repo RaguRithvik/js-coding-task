7f4b78706afea1540fc64a4c64bf4917f9bb9ee4
ragurithvik <ragurithvik@gmail.com>
2025-10-12 12:19:55 +0530

    code

--- practices.js
+++ practices.js
@@ -1,1 +1,9 @@
-//Find the second largest number in an array
+//01.Find the second largest number in an array-run ctrl+b
+
+function seclrgNumArr(arr){
+for(a of arr){
+	console.log(a)
+}
+}
+console.log(seclrgNumArr([2, 4, 5 , 100, -1, 10]))
+
